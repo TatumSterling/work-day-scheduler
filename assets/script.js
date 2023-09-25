ed288294212ed99bfe17a23ee8de80472419e8bd
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-24 23:07:31 -0500

    condensed local storage functions

--- assets/script.js
+++ assets/script.js
@@ -31,44 +31,28 @@ $(function () {
 }) 
 
 saveBtn.on("click", saveUserInput);
-// TODO need to figure out how to condense these funtions
-// so I don't repeat myself, maybe for loop 
 
 //sets local storage key value pairs
 function saveUserInput () {
 
-  localStorage.setItem('9AM', $('#hour9').val());
-  localStorage.setItem('10AM', $('#hour10').val());
-  localStorage.setItem('11AM', $('#hour11').val());
-  localStorage.setItem('12PM', $('#hour12').val());
-  localStorage.setItem('1PM', $('#hour13').val());
-  localStorage.setItem('2PM', $('#hour14').val());
-  localStorage.setItem('3PM', $('#hour15').val());
-  localStorage.setItem('4PM', $('#hour16').val());
-  localStorage.setItem('5PM', $('#hour17').val())
+  $("textarea").each(function() {
+    var hour = $(this).attr("id");
+    var task = $(this).val();
+
+    localStorage.setItem(hour, task);
+  })
 }
 
-
-// stores user input into a variable
-var hour9 = $("#hour9").text();
-var hour10 = $("#hour10").text();
-var hour11 = $("#hour11").text();
-var hour12 = $("#hour12").text();
-var hour13 = $("#hour13").text();
-var hour14 = $("#hour14").text();
-var hour15 = $("#hour15").text();
-var hour16 = $("#hour16").text();
-var hour17 = $("#hour17").text();
+//loops through index 9-17 and gets user input for each
+var hours = {};
 
 
-// if there is content in local storage this will display in 
-//correct time box el
-$("#hour9").html(localStorage.getItem("9AM"));
-$("#hour10").html(localStorage.getItem("10AM"));
-$("#hour11").html(localStorage.getItem("11AM"));
-$("#hour12").html(localStorage.getItem("12PM"));
-$("#hour13").html(localStorage.getItem("1PM"));
-$("#hour14").html(localStorage.getItem("2PM"));
-$("#hour15").html(localStorage.getItem("3PM"));
-$("#hour16").html(localStorage.getItem("4PM"));
-$("#hour17").html(localStorage.getItem("5PM"));
+for (var i = 9; i <= 17; i++) {
+  hours["hour" + i] = $("#hour" + i).text();
+}
+
+// loops through index 9-17 and retrieves from local storage if any
+for(var i = 9; i<= 17; i++){
+  $("#hour"+i).html(localStorage.getItem("hour"+i));
+}
+
