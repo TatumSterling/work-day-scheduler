9a089d2762ec6ad87488ca7cf2bb34dce59bef4e
Tatum Sterling <tatum.sterling29@gmail.com>
2023-09-24 22:48:13 -0500

    added local storage functions, need to condense them

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-
+var saveBtn = $(".btn")
 
 
 
@@ -20,10 +14,11 @@ $(function () {
   // grabs all elements with class name time-block 
 
   $(".time-block").each(function () {
-    var timeBlockHour=(this.id);
+    var timeBlockHour=(this.id);  //targets id of each element in that class
 
-    console.log(timeBlockHour)
 
+    // checks to see if time block hour is past, present, or future
+    // and adds corresponding class
     if(timeBlockHour < now) {
       $(this).addClass("past");
     } else if(timeBlockHour == now) {
@@ -35,15 +30,45 @@ $(function () {
   })
 }) 
 
+saveBtn.on("click", saveUserInput);
+// TODO need to figure out how to condense these funtions
+// so I don't repeat myself, maybe for loop 
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
+//sets local storage key value pairs
+function saveUserInput () {
+
+  localStorage.setItem('9AM', $('#hour9').val());
+  localStorage.setItem('10AM', $('#hour10').val());
+  localStorage.setItem('11AM', $('#hour11').val());
+  localStorage.setItem('12PM', $('#hour12').val());
+  localStorage.setItem('1PM', $('#hour13').val());
+  localStorage.setItem('2PM', $('#hour14').val());
+  localStorage.setItem('3PM', $('#hour15').val());
+  localStorage.setItem('4PM', $('#hour16').val());
+  localStorage.setItem('5PM', $('#hour17').val())
+}
+
+
+// stores user input into a variable
+var hour9 = $("#hour9").text();
+var hour10 = $("#hour10").text();
+var hour11 = $("#hour11").text();
+var hour12 = $("#hour12").text();
+var hour13 = $("#hour13").text();
+var hour14 = $("#hour14").text();
+var hour15 = $("#hour15").text();
+var hour16 = $("#hour16").text();
+var hour17 = $("#hour17").text();
+
+
+// if there is content in local storage this will display in 
+//correct time box el
+$("#hour9").html(localStorage.getItem("9AM"));
+$("#hour10").html(localStorage.getItem("10AM"));
+$("#hour11").html(localStorage.getItem("11AM"));
+$("#hour12").html(localStorage.getItem("12PM"));
+$("#hour13").html(localStorage.getItem("1PM"));
+$("#hour14").html(localStorage.getItem("2PM"));
+$("#hour15").html(localStorage.getItem("3PM"));
+$("#hour16").html(localStorage.getItem("4PM"));
+$("#hour17").html(localStorage.getItem("5PM"));
